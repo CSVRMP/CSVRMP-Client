984e18ad8ba0df9fbe248085bcdd29f3a64b475d
Aaron <aaronfrancis635@gmail.com>
2021-08-04 22:32:01 +0100

    Started Project

---
+++ CSVRMP-Client/CSVRMP.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVRMP_Client
+{
+    public class CSVRMP : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Warning("Game might lag for a bit.");
+            MelonLogger.Msg("Setting up GameObject tracking");
+        }
+    }
+}
